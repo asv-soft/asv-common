19e21dc5b8c44120b0154c2450c8e849e51b6eba
Alexey <248997@gmail.com>
2023-02-27 18:10:09 +0500

    [Asv.Common] IncrementalRateCounter added

--- src/Asv.IO/Streams/IDataStream.cs
+++ src/Asv.IO/Streams/IDataStream.cs
@@ -17,19 +17,19 @@ namespace Asv.IO
     {
         public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,  CancellationToken cancel = default)
         {
-            return Send(src, data, out var sended, cancel);
+            return Send(src, data, out var byteSent, cancel);
         }
 
-        public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,out int byteSended, CancellationToken cancel = default)
+        public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,out int byteSent, CancellationToken cancel = default)
         {
             var size = data.GetByteSize();
             var array = ArrayPool<byte>.Shared.Rent(size);
             var span = new Span<byte>(array,0,size);
             try
             {
-                byteSended = span.Length;
+                byteSent = span.Length;
                 data.Serialize(ref span);
-                byteSended -= span.Length;
+                byteSent -= span.Length;
                 return src.Send(array, size, cancel);
             }
             finally
