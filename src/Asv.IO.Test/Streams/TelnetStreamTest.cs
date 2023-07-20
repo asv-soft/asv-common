8fff392bb4e3a5d0486a08343e656f3cdd743e14
asv-soft-u2 <u02@asv.me>
2023-07-20 17:28:07 +0300

    fix: add whitespace

--- src/Asv.IO.Test/Streams/TelnetStreamTest.cs
+++ src/Asv.IO.Test/Streams/TelnetStreamTest.cs
@@ -35,13 +35,13 @@ namespace Asv.IO.Test.Streams
         {
             using var port1 = PortFactory.Create("tcp://127.0.0.1:55000", true);
             using var port2 = PortFactory.Create("tcp://127.0.0.1:55000?srv=true", true);
-            using var strm1 = new TelnetStream(port1, Encoding.ASCII);
+            using var strm1 = new TelnetStream(port1, Encoding.ASCII, 10);
             using var strm2 = new TelnetStream(port2, Encoding.ASCII, 10);
             while (port1.State.Value != PortState.Connected && port2.State.Value != PortState.Connected)
             {
                 await Task.Delay(1000);
             }
-
+ 
             var tcs = new TaskCompletionSource<Exception>();
             
             using var a = strm2.OnError.FirstAsync().Subscribe(_=> tcs.SetResult(_));
