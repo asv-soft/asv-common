ba85f9eaa9ad20493d234999ec57226b07f209ca
asv-soft-u02 <u02@asv.me>
2024-09-16 22:13:21 +0300

    add: Logged Data

--- src/Asv.IO/ULog/Tokens/ULogLoggedDataMessageToken.cs
+++ src/Asv.IO/ULog/Tokens/ULogLoggedDataMessageToken.cs
@@ -17,31 +17,31 @@ public class ULogLoggedDataMessageToken : IULogToken
     public TokenPlaceFlags Section => TokenPlaceFlags.Data;
 
     /// <summary>
-    /// MessageId: unique id to match Logged data Message data. The first use must set this to 0, then increase it.
+    /// msg_id: unique id to match Logged data Message data. The first use must set this to 0, then increase it.
     /// 
     /// The same msg_id must not be used twice for different subscriptions.
     /// </summary>
     public ushort MessageId { get; set; }
-    
+
     /// <summary>
-    /// Data contains the logged binary message as defined by Format Message
+    /// data contains the logged binary message as defined by Format Message
     /// </summary>
-    public byte Data { get; set; }
+    public FormatMessageField Data { get; set; } = new();
     
     public void Deserialize(ref ReadOnlySpan<byte> buffer)
     {
-        throw new NotImplementedException();
+        MessageId = BitConverter.ToUInt16(BinSerialize.ReadBlock(ref buffer, 2));
+        Data.Deserialize(ref buffer);
     }
 
     public void Serialize(ref Span<byte> buffer)
     {
-        throw new NotImplementedException();
+        BinSerialize.WriteBlock(ref buffer, BitConverter.GetBytes(MessageId));
+        Data.Serialize(ref buffer);
     }
 
     public int GetByteSize()
     {
-        return sizeof(ushort);
+        return sizeof(ushort) + Data.GetByteSize();
     }
-
-
 }
