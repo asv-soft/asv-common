eabae4df48849330cc40c5a2438f2a7cb62f7a36
Maxim <u04@asv.me>
2024-09-17 15:21:04 +0300

    feat(ulog): small refactor subscription token and add tests
    asana: https://app.asana.com/0/1203851531040615/1208271533186867/f

--- src/Asv.IO.Test/ULog/ULogSubscriptionMessageToken.Tests.cs
+++ src/Asv.IO.Test/ULog/ULogSubscriptionMessageToken.Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Buffers;
 using Xunit;
 using Xunit.Abstractions;
@@ -12,25 +13,25 @@ public class ULogSubscriptionMessageTokenTests
     {
         _output = output;
     }
-    
+
     [Fact]
     public void ReadHeader()
     {
         var data = new ReadOnlySequence<byte>(TestData.ulog_log_small);
-        var rdr = new SequenceReader<byte>(data); 
+        var rdr = new SequenceReader<byte>(data);
         var reader = ULog.CreateReader();
 
         var result = reader.TryRead<ULogFileHeaderToken>(ref rdr, out var header);
         Assert.True(result);
         Assert.NotNull(header);
-        Assert.Equal(ULogToken.FileHeader,header.Type);
+        Assert.Equal(ULogToken.FileHeader, header.Type);
         Assert.Equal(20309082U, header.Timestamp);
-        Assert.Equal(1,header.Version);
+        Assert.Equal(1, header.Version);
 
         result = reader.TryRead<ULogFlagBitsMessageToken>(ref rdr, out var flag);
         Assert.True(result);
-        Assert.NotNull(flag);  
-        Assert.Equal(ULogToken.FlagBits,flag.Type);
+        Assert.NotNull(flag);
+        Assert.Equal(ULogToken.FlagBits, flag.Type);
 
         while (reader.TryRead(ref rdr, out var token))
         {
@@ -38,8 +39,168 @@ public class ULogSubscriptionMessageTokenTests
             if (token.Type == ULogToken.Subscription)
             {
                 var format = token as ULogSubscriptionMessageToken;
-                _output.WriteLine($"Format: {format.Type:G} {string.Join(",",format.Fields.MessageId)} {string.Join(",",format.Fields.MultiId)} {string.Join(",",format.Fields.MessageName)}");    
+                _output.WriteLine(
+                    $"{format.Type:G}: {string.Join(",", format.Fields.MessageName)} ({string.Join(",", format.Fields.MultiId)} {string.Join(",", format.Fields.GetByteSize())})");
             }
-        } 
+        }
     }
+
+    # region Deserialize
+
+    [Theory]
+    [InlineData(1, 14, "data")]
+    [InlineData(byte.MaxValue, ushort.MaxValue, "data")]
+    [InlineData(byte.MinValue, ushort.MinValue, "data")]
+    [InlineData(byte.MaxValue, ushort.MinValue, "data")]
+    [InlineData(byte.MinValue, ushort.MaxValue, "data")]
+    public void DeserializeToken_Success(byte multiId, ushort messageId, string messageName)
+    {
+        // Arrange
+        var readOnlySpan = SetUpTestData(multiId, messageId, messageName);
+        var token = new ULogSubscriptionMessageToken();
+
+        // Act
+        token.Deserialize(ref readOnlySpan);
+
+        // Assert
+        Assert.Equal(multiId, token.Fields.MultiId);
+        Assert.Equal(messageId, token.Fields.MessageId);
+        Assert.Equal(messageName, token.Fields.MessageName);
+    }
+
+    [Theory]
+    [InlineData(1, 14, null)]
+    [InlineData(byte.MaxValue, ushort.MaxValue, null)]
+    [InlineData(byte.MinValue, ushort.MinValue, null)]
+    public void DeserializeToken_NoMessage(byte multiId, ushort messageId, string messageName)
+    {
+        Assert.Throws<ULogException>(() =>
+        {
+            var readOnlySpan = SetUpTestData(multiId, messageId, messageName);
+            var token = new ULogSubscriptionMessageToken();
+            token.Deserialize(ref readOnlySpan);
+        });
+    }
+
+    [Theory]
+    [InlineData(1, 14, "%@#")]
+    [InlineData(byte.MaxValue, ushort.MaxValue, "`!!!`````````")]
+    [InlineData(byte.MinValue, ushort.MinValue, "")]
+    public void DeserializeToken_WrongMessageName(byte multiId, ushort messageId, string messageName)
+    {
+        Assert.Throws<ULogException>(() =>
+        {
+            var readOnlySpan = SetUpTestData(multiId, messageId, messageName);
+            var token = new ULogSubscriptionMessageToken();
+            token.Deserialize(ref readOnlySpan);
+        });
+    }
+
+    # endregion
+
+    # region Serialize
+
+    [Theory]
+    [InlineData(1, 14, "data")]
+    [InlineData(byte.MaxValue, ushort.MaxValue, "data")]
+    [InlineData(byte.MinValue, ushort.MinValue, "data")]
+    [InlineData(byte.MaxValue, ushort.MinValue, "data")]
+    [InlineData(byte.MinValue, ushort.MaxValue, "data")]
+    public void SerializeToken_Success(byte multiId, ushort messageId, string messageName)
+    {
+        // Arrange
+        var readOnlySpan = SetUpTestData(multiId, messageId, messageName);
+        var token = SetUpTestToken(multiId, messageId, messageName);
+
+        // Act
+        var span = new Span<byte>(new byte[readOnlySpan.Length]);
+        var temp = span;
+        token.Serialize(ref temp);
+
+        // Assert
+        Assert.True(span.SequenceEqual(readOnlySpan));
+    }
+
+    [Theory]
+    [InlineData(1, 14, "%@#")]
+    [InlineData(byte.MaxValue, ushort.MaxValue, "`!!!`````````")]
+    [InlineData(byte.MinValue, ushort.MinValue, "")]
+    public void SerializeToken_WrongMessageName(byte multiId, ushort messageId, string messageName)
+    {
+        Assert.Throws<ULogException>(() =>
+        {
+            var readOnlySpan = SetUpTestData(multiId, messageId, messageName);
+            var token = SetUpTestToken(multiId, messageId, messageName);
+            var span = new Span<byte>(new byte[readOnlySpan.Length]);
+            var temp = span;
+            token.Serialize(ref temp);
+        });
+    }
+
+    # endregion
+
+    # region GetByteSize
+
+    [Theory]
+    [InlineData(1, 14, "data")]
+    [InlineData(byte.MaxValue, ushort.MaxValue, "data")]
+    [InlineData(byte.MinValue, ushort.MinValue, "data")]
+    [InlineData(byte.MaxValue, ushort.MinValue, "data")]
+    [InlineData(byte.MinValue, ushort.MaxValue, "data")]
+    public void GetByteSize_Success(byte multiId, ushort messageId, string messageName)
+    {
+        // Arrange
+        var setup = SetUpTestData(multiId, messageId, messageName);
+        var token = SetUpTestToken(multiId, messageId, messageName);
+
+        // Act
+        var size = token.GetByteSize();
+
+        // Assert
+        Assert.Equal(setup.Length, size);
+    }
+
+    # endregion
+
+    #region Setup
+
+    private ULogSubscriptionMessageToken SetUpTestToken(byte multiId, ushort messageId, string messageName)
+    {
+        var token = new ULogSubscriptionMessageToken();
+        token.Fields = new SubscriptionMessageFields()
+        {
+            MessageId = messageId,
+            MultiId = multiId,
+            MessageName = messageName
+        };
+
+        return token;
+    }
+
+    private ReadOnlySpan<byte> SetUpTestData(byte multiId, ushort messageId, string messageName)
+    {
+        if (messageName is null)
+        {
+            throw new ULogException("Value cannot be null");
+        }
+
+        var messageIdBytes = BitConverter.GetBytes(messageId);
+        var messageNameBytes = ULog.Encoding.GetBytes(messageName);
+        var buffer = new Span<byte>(new byte[1 + 2 + messageNameBytes.Length]);
+
+        buffer[0] = multiId;
+        messageIdBytes.CopyTo(buffer.Slice(1, 2));
+
+        for (var i = 0; i < messageNameBytes.Length; i++)
+        {
+            buffer[i + 3] = messageNameBytes[i];
+        }
+
+        var byteArray = buffer.ToArray();
+        var readOnlySpan = new ReadOnlySpan<byte>(byteArray);
+
+        return readOnlySpan;
+    }
+
+    #endregion
 }
--- src/Asv.IO/ULog/Tokens/ULogSubscriptionMessageToken.cs
+++ src/Asv.IO/ULog/Tokens/ULogSubscriptionMessageToken.cs
@@ -9,7 +9,7 @@ namespace Asv.IO;
 /// Subscribe a message by name and give it an id that is used in Logged data Message.
 /// This must come before the first corresponding Logged data Message.
 /// </summary>
-public class ULogSubscriptionMessageToken: IULogToken
+public class ULogSubscriptionMessageToken : IULogToken
 {
     public static ULogToken Token => ULogToken.Subscription;
     public const string TokenName = "Subscription";
@@ -18,13 +18,14 @@ public class ULogSubscriptionMessageToken: IULogToken
     public string Name => TokenName;
     public ULogToken Type => Token;
     public TokenPlaceFlags Section => TokenPlaceFlags.Definition | TokenPlaceFlags.Data;
-    public SubscriptionMessageField Fields { get; set; } = null!;
+    public SubscriptionMessageFields Fields { get; set; } = null!;
+
     public void Deserialize(ref ReadOnlySpan<byte> buffer)
     {
-        Fields = new SubscriptionMessageField
+        Fields = new SubscriptionMessageFields
         {
             MultiId = BinSerialize.ReadByte(ref buffer),
-            MessageId = BinSerialize.ReadShort(ref buffer)
+            MessageId = BinSerialize.ReadUShort(ref buffer)
         };
         Fields.Deserialize(ref buffer);
         buffer = buffer[Fields.MessageName.Length..];
@@ -33,7 +34,6 @@ public class ULogSubscriptionMessageToken: IULogToken
     public void Serialize(ref Span<byte> buffer)
     {
         Fields.Serialize(ref buffer);
-        
     }
 
     public int GetByteSize()
@@ -42,24 +42,24 @@ public class ULogSubscriptionMessageToken: IULogToken
     }
 }
 
-public class SubscriptionMessageField : ISizedSpanSerializable
+public class SubscriptionMessageFields : ISizedSpanSerializable
 {
     private string _messageName;
-    
+
     /// <summary>
     /// The same message format can have multiple instances, for example if the system has two sensors of the same type.
     /// 
     /// The default and first instance must be 0.
     /// </summary>
     public byte MultiId { get; set; }
-    
+
     /// <summary>
     /// Unique id to match Logged data Message data. The first use must set this to 0, then increase it.
     ///
     /// The same msg_id must not be used twice for different subscriptions.
     /// </summary>
-    public Int16 MessageId { get; set; }
-    
+    public ushort MessageId { get; set; }
+
     /// <summary>
     /// Message name to subscribe to. Must match one of the Format Message definitions.
     /// </summary>
@@ -72,31 +72,34 @@ public class SubscriptionMessageField : ISizedSpanSerializable
             _messageName = value;
         }
     }
-    
+
     public int GetByteSize()
     {
-        return sizeof(byte) + sizeof(Int16) + ULog.Encoding.GetByteCount(MessageName);
+        return sizeof(byte) + sizeof(ushort) + ULog.Encoding.GetByteCount(MessageName);
     }
 
     public void Deserialize(ref ReadOnlySpan<char> rawString)
     {
         MessageName = rawString.Trim().ToString();
     }
+
     public void Deserialize(ref ReadOnlySpan<byte> buffer)
     {
         var charSize = ULog.Encoding.GetCharCount(buffer);
         var charBuffer = new char[charSize];
-        ULog.Encoding.GetChars(buffer,charBuffer);
+        ULog.Encoding.GetChars(buffer, charBuffer);
         var rawString = new ReadOnlySpan<char>(charBuffer, 0, charSize);
         Deserialize(ref rawString);
     }
 
     public void Serialize(ref Span<byte> buffer)
     {
+        CheckName(MessageName);
         BinSerialize.WriteByte(ref buffer, MultiId);
-        BinSerialize.WriteShort(ref buffer, MessageId);
+        BinSerialize.WriteUShort(ref buffer, MessageId);
         BinSerialize.WriteBlock(ref buffer, ULog.Encoding.GetBytes(MessageName));
     }
+
     private void CheckName(string? name)
     {
         ULog.CheckMessageName(name);
