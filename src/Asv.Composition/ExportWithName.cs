7416447345addaf1652a3fbfb051bd474aa307fd
Alexey <248997@gmail.com>
2024-10-01 10:14:30 +0500

    feat(mef2): add ExportWithNameAsContractAttribute

--- src/Asv.Composition/ExportWithName.cs
+++ src/Asv.Composition/ExportWithName.cs
@@ -7,6 +7,23 @@ namespace Asv.Composition;
 public class ExportWithNameAttribute : ExportAttribute,INameMetadata
 {
     public ExportWithNameAttribute(Type type, string name, int priority)
+        :base(type)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            ArgumentException.ThrowIfNullOrWhiteSpace(nameof(name));
+        Name = name;
+        Priority = priority;
+    }
+
+    public string Name { get; }
+    public int Priority { get; }
+}
+
+[MetadataAttribute]
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class ExportWithNameAsContractAttribute : ExportAttribute,INameMetadata
+{
+    public ExportWithNameAsContractAttribute(Type type, string name, int priority)
         :base(name,type)
     {
         if (string.IsNullOrWhiteSpace(name))
