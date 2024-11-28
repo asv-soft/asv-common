466a9eff4ffc4f594aeb1ceced2be85fb4186eab
Alexey <248997@gmail.com>
2024-11-28 10:15:34 +0500

    feat(io): add IProtocolFactory methods

--- src/Asv.IO/Protocol/IProtocol.cs
+++ src/Asv.IO/Protocol/IProtocol.cs
@@ -23,8 +23,19 @@ public interface IProtocolContext
 
 public interface IProtocolFactory
 {
+    ILoggerFactory LoggerFactory { get; }
+    TimeProvider TimeProvider { get; }
+    IMeterFactory MeterFactory { get; }
+    ImmutableArray<ProtocolInfo> AvailableProtocols { get; }
+    ImmutableArray<PortTypeInfo> AvailablePortTypes { get; }
+    ImmutableArray<IProtocolMessageFormatter> Formatters { get; }
+    ImmutableArray<IProtocolFeature> Features { get; }
+    IStatistic Statistic { get; }
+    IProtocolPort CreatePort(string id);
+    IProtocolParser CreateParser(string id);
     IProtocolRouter CreateRouter(string id);
     IVirtualConnection CreateVirtualConnection(Func<IProtocolMessage, bool>? clientToServerFilter = null,
         Func<IProtocolMessage, bool>? serverToClientFilter = null);
+    
     
 }
