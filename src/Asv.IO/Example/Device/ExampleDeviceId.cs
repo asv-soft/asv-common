e42248e21967c2aa6ddf599415a35a01d1dfd5d1
Alexey <248997@gmail.com>
2024-12-03 19:47:28 +0500

    fix(io): move ExampleDeviceId to a separate file

---
+++ src/Asv.IO/Example/Device/ExampleDeviceId.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Asv.IO.Device;
+
+public class ExampleDeviceId(string @class, byte targetId) : DeviceId(@class), IEquatable<ExampleDeviceId>
+{
+    public bool Equals(ExampleDeviceId? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return base.Equals(other) && TargetId == other.TargetId;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return ReferenceEquals(this, obj) || obj is ExampleDeviceId other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(base.GetHashCode(), TargetId);
+    }
+
+    public static bool operator ==(ExampleDeviceId? left, ExampleDeviceId? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ExampleDeviceId? left, ExampleDeviceId? right)
+    {
+        return !Equals(left, right);
+    }
+
+    public byte TargetId { get; } = targetId;
+
+    public override string AsString() => $"{DeviceClass}.{TargetId}";
+}
