2589a3aaddbf870c091ccbf2878f9c23de430fb3
Alexey <248997@gmail.com>
2025-04-05 11:07:08 +0500

    feat(io): simplify ProtocolDeserializeMessageException constructors

--- src/Asv.IO/Protocol/Parser/Exceptions/ProtocolDeserializeMessageException.cs
+++ src/Asv.IO/Protocol/Parser/Exceptions/ProtocolDeserializeMessageException.cs
@@ -5,13 +5,25 @@ namespace Asv.IO;
 public class ProtocolDeserializeMessageException : ProtocolParserException
 {
     public ProtocolDeserializeMessageException(ProtocolInfo parser, IProtocolMessage message, Exception ex) 
-        : base(parser, $"Deserialization {parser}.{message.Name} message error.",ex)
+        : this(parser, message.Name,ex)
+    {
+        
+    }
+    
+    public ProtocolDeserializeMessageException(ProtocolInfo parser, string messageName, Exception ex) 
+        : base(parser, $"Deserialization {parser}.{messageName} message error.",ex)
     {
         
     }
     
     public ProtocolDeserializeMessageException(ProtocolInfo parser, IProtocolMessage message, string messageError) 
-        : base(parser, $"Deserialization {parser}.{message.Name} message error:{messageError}")
+        : this(parser, message.Name, messageError)
+    {
+        
+    }
+    
+    public ProtocolDeserializeMessageException(ProtocolInfo parser, string messageName, string messageError) 
+        : base(parser, $"Deserialization {parser}.{messageName} message error:{messageError}")
     {
         
     }
