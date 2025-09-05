ebdc66679206bc4d749c206318fd0602351d6946
Alexey <248997@gmail.com>
2025-09-05 19:15:22 +0500

    feat(statistics): add PrintTable extension method for formatted output

--- src/Asv.IO/Protocol/Statistic/IStatistic.cs
+++ src/Asv.IO/Protocol/Statistic/IStatistic.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Buffers;
 using Asv.Common;
+using DotNext.Buffers;
 using Microsoft.Extensions.Logging;
 using ZLogger;
 
@@ -42,6 +45,37 @@ public static class StatisticHelper
         logger.ZLogDebug(
             $"Parsed[msg:{src.ParsedMessages}, bytes:{src.ParsedBytes.BytesToString()}, pub_err:{src.MessagePublishError}, unknown:{src.UnknownMessages}, crc:{src.BadCrcError}, deserialize:{src.DeserializeError}, read:{src.MessageReadNotAllData}]");
     }
+    
+    public static string PrintTable(this IStatistic stat)
+    {
+        using var writer = new PoolingArrayBufferWriter<char>(ArrayPool<char>.Shared);
+        const int nameWidth = 24;
+
+        void AddLine(string name, uint value)
+        {
+            var line = $"{name,-nameWidth} {value}\n";
+            writer.Write(line.AsSpan());
+        }
+
+        AddLine(nameof(stat.RxBytes), stat.RxBytes);
+        AddLine(nameof(stat.TxBytes), stat.TxBytes);
+        AddLine(nameof(stat.RxMessages), stat.RxMessages);
+        AddLine(nameof(stat.TxMessages), stat.TxMessages);
+        AddLine(nameof(stat.RxError), stat.RxError);
+        AddLine(nameof(stat.TxError), stat.TxError);
+        AddLine(nameof(stat.DroppedRxMessages), stat.DroppedRxMessages);
+        AddLine(nameof(stat.DroppedTxMessages), stat.DroppedTxMessages);
+        AddLine(nameof(stat.ParsedBytes), stat.ParsedBytes);
+        AddLine(nameof(stat.ParsedMessages), stat.ParsedMessages);
+        AddLine(nameof(stat.UnknownMessages), stat.UnknownMessages);
+        AddLine(nameof(stat.MessagePublishError), stat.MessagePublishError);
+        AddLine(nameof(stat.BadCrcError), stat.BadCrcError);
+        AddLine(nameof(stat.DeserializeError), stat.DeserializeError);
+        AddLine(nameof(stat.MessageReadNotAllData), stat.MessageReadNotAllData);
+
+        return writer.ToString();
+    }
+    
 }
 
 public interface ISupportStatistic
