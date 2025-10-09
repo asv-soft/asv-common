86e96ec5c4395f62b44f784273332539d7ce0084
asvol <248997@gmail.com>
2025-10-10 03:12:41 +0500

    feat: add CircularBufferMixin for dynamic buffer capacity management

---
+++ src/Asv.Common/Collections/CircularBufferMixin.cs
@@ -0,0 +1,32 @@
+﻿using Asv.Common;
+
+namespace Asv.Common;
+
+public static class CircularBufferMixin
+{
+    public static void EnsureCapacity<T>(ref CircularBuffer2<T> buffer, int capacity)
+    {
+        if (buffer.Size > capacity)
+        {
+            var newBuffer = new CircularBuffer2<T>(capacity);
+            for (var i = 0; i < capacity; i++)
+            {
+                newBuffer.PushFront(buffer[i]);
+            }
+
+            buffer = newBuffer;
+        }
+
+        if (buffer.Size < capacity)
+        {
+            var newBuffer = new CircularBuffer2<T>(capacity);
+            for (var i = 0; i < buffer.Size; i++)
+            {
+                newBuffer.PushFront(buffer[i]);
+            }
+
+            buffer = newBuffer;
+        }
+
+    }
+}
